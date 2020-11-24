df1b948dbc14daa61b198e8fa57fe2dbfbb6bf1f
SarahAsliPolat <asliatamanpolat@gmail.com>
2020-11-24 10:54:34 -0500

    welcome to cucumber

---
+++ SelfPractice/src/test/java/com/cybertekpractice/tests/Day1_selenium_intro/GItPractice.java
@@ -0,0 +1,7 @@
+package com.cybertekpractice.tests.Day1_selenium_intro;
+
+public class GItPractice {
+    public static void main(String[] args) {
+        System.out.println("git ogrenmece");
+    }
+}
